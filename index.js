f71434e36ddbc777433f219bab8dc224c0e0d5df
locallab-seoul <locallab.seoul@gmail.com>
2021-12-12 15:17:24 +0900

    두번째 커밋

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
 const express = require('express')
 const app = express()
 const port = 8000
+// const bodyParser = require('body-parser')
+
+const config = require('./config/key')
+const { User } = require('./models/User')
+
+// application/x-www-form-urllencoded 데이터를 분석해서 가져올 수 있게?
+// app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }))
+
+// application/json 을 분석해서 가져올 수 있게?
+// app.use(bodyParser.json())
+app.use(express.json())
+
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://locallab:locallab0319!@bolierplate.iizxx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+mongoose.connect(config.mongoURI)
 .then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! by Nodemon')
+})
+
+app.post('/register', (req, res) => {
+    // 회원가입할때 필요한 정보들을 client에서 가져오면
+    // 그것들을 데이터베이스에 넣는다
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
+
 })
 
 app.listen(port, () => {
