8218e1bd4914dcc27c4fd81e39ac829fba4e7596
locallab-seoul <locallab.seoul@gmail.com>
2022-01-02 22:28:39 +0900

    #13 Auth 기능 만들기

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = 8000
 const cookieParser = require('cookie-parser')
 
 const config = require('./config/key')
+const { auth } = require('./middleware/auth')
 const { User } = require('./models/User')
 
 // application/x-www-form-urllencoded 데이터를 분석해서 가져올 수 있게?
@@ -26,7 +27,7 @@ app.get('/', (req, res) => {
   res.send('Hello World! by Nodemon')
 })
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
     // 회원가입할때 필요한 정보들을 client에서 가져오면
     // 그것들을 데이터베이스에 넣는다
 
@@ -41,7 +42,7 @@ app.post('/register', (req, res) => {
 
 })
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
     // 요청된 이메일을 데이터베이스에서 찾는다
     User.findOne({"email": req.body.email}, (err, user) => {
         if(!user) {
@@ -68,6 +69,21 @@ app.post('/login', (req, res) => {
                 .json({ loginSuccess: true, userId: user._id })
             })
         })
+    })
+})
+
+app.get('/api/users/auth', auth, (req, res) => {
+    
+    // 여기까지 미들웨어(auth)를 통과해 왔다는 얘기는 Authentication이 True 라는 말
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
     })
 
 })
