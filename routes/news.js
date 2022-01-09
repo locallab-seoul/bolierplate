a8febac06c278df6bcba41aae80ed4708652bd90
locallab-seoul <locallab.seoul@gmail.com>
2022-01-09 22:25:57 +0900

    space, news 이미지 업로드 api 구현 (multer)

--- routes/news.js
+++ routes/news.js
@@ -1,7 +1,32 @@
-
 const express = require('express')
 const router = express.Router()
+const multer = require('multer')
 const { News } = require('../models/News')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, `${Date.now()}_${file.originalname}`)
+    }
+  })
+  
+const upload = multer({ storage: storage }).single("file")
+
+router.post('/image', (req, res) => {
+
+    // 가져온 이미지를 저장 해주면 된다.
+    upload(req, res, err => {
+        if(err) {
+            return res.json({success: false, err})
+        }
+        return res.json({success: true, filePath: res.req.file.path, fileName: res.req.filename })
+    })
+
+})
+
 
 router.post('/create', (req, res) => {
 
